070cec58cf4ce46e95ff9a5632334ec8c3da6b5b
Zahliada VM <waah777@gmail.com>
2025-04-30 11:44:42 +0300

    end 072.1 Заготовка под проект

---
+++ proj.ceramic_soul/.prettierrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+	// 0 = off, 1 = warn, 2 = error
+	tabWidth: 2,
+	useTabs: true,
+	quoteProps: 'as-needed',
+	singleQuote: true,
+	jsxSingleQuote: true,
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	bracketSameLine: false,
+	arrowParens: 'always',
+	maxLen: ['warn', 140, 2],
+	semi: true,
+	embeddedLanguageFormatting: 'auto',
+	// linebreakStyle: ['off', 'windows'],
+	plugins: [
+		// 'prettier-plugin-organize-imports',
+		'prettier-plugin-organize-attributes',
+		'prettier-plugin-css-order',
+	],
+	organizeImportsSkipDestructiveCodeActions: true,
+	// htmlWhitespaceSensitivity: 'css',
+	// attributeGroups: ['^class$', '^(id|name)$', '$DEFAULT', '^aria-'],
+	// attributeSort: 'ASC',
+};
