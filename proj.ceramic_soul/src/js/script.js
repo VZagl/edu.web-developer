877ad3d6d15756068799cd966360a92ed6e04bae
Zahliada VM <waah777@gmail.com>
2025-05-23 11:34:38 +0300

    end 084.1 Знакомимся с языком программирования Javascript

--- proj.ceramic_soul/src/js/script.js
+++ proj.ceramic_soul/src/js/script.js
@@ -4,3 +4,14 @@
 // import '../scss/style.scss';
 
 console.log('This is the `index` page');
+
+document.addEventListener('DOMContentLoaded', () => {
+	console.log('DOM fully loaded and parsed');
+
+	const vase = document.querySelector('.touch__decor');
+	console.log('vase =', vase);
+	vase.addEventListener('click', () => {
+		console.log('vase clicked');
+		// vase.classList.toggle('active');
+	});
+});
