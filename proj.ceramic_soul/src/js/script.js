307fa663a555e327c6db392db4c6c53acad5b505
Zahliada VM <waah777@gmail.com>
2025-05-30 16:54:37 +0300

    end 086.1 Большая практика. Создаем и разбираем компонент-слайдер. Часть 2

--- proj.ceramic_soul/src/js/script.js
+++ proj.ceramic_soul/src/js/script.js
@@ -1,17 +1,40 @@
-// import 'purecss/build/grids-min.css';
-// import 'purecss/build/grids-responsive-min.css';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 
-// import '../scss/style.scss';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import '/src/scss/style.scss';
 
 console.log('This is the `index` page');
 
 document.addEventListener('DOMContentLoaded', () => {
 	console.log('DOM fully loaded and parsed');
 
-	const vase = document.querySelector('.touch__decor');
-	console.log('vase =', vase);
-	vase.addEventListener('click', () => {
-		console.log('vase clicked');
-		// vase.classList.toggle('active');
-	});
+	try {
+		new Swiper('.works__slider', {
+			slidesPerView: 1,
+			loop: true,
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+			navigation: {
+				nextEl: '.icon-right-open',
+				prevEl: '.icon-left-open',
+			},
+			breakpoints: {
+				// when window width is >= 1200px
+				1200: {
+					slidesPerView: 3,
+					spaceBetween: 5,
+				},
+				1920: {
+					spaceBetween: 35,
+				},
+			},
+			modules: [Navigation, Pagination],
+		});
+	} catch (e) {}
 });
