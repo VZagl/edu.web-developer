13d94bde8638e47f6fde00c421a772c21afbf281
Zahliada VM <waah777@gmail.com>
2025-08-08 15:07:39 +0300

    Добавить иконки и метаданные

---
+++ proj.esl-pro/src/js/script.js
@@ -0,0 +1,7 @@
+import '/src/scss/style.scss';
+
+console.log('This is the `index` page');
+
+document.addEventListener('DOMContentLoaded', () => {
+	console.log('DOM fully loaded and parsed');
+});
