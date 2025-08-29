f05d77d212434a6c42d4a2381e65b533ad9c4bba
Zahliada VM <waah777@gmail.com>
2025-08-29 14:55:15 +0300

    обновление конфигурации и зависимостей проекта
    - Добавлен модуль `@types/node` версии ^24.3.0 в devDependencies.
    - Обновлен файл `pnpm-lock.yaml` для синхронизации с изменениями зависимостей.
    - Внесены изменения в `vite.config.ts`:
      - Добавлен алиас для стилей: `styles` указывает на `src/styles`.
    
    Эти изменения улучшают поддержку TypeScript и упрощают работу с путями в проекте.

--- proj.portfolio/vite.config.ts
+++ proj.portfolio/vite.config.ts
@@ -1,8 +1,17 @@
 import react from '@vitejs/plugin-react';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
+	resolve: {
+		alias: {
+			styles: resolve(__dirname, 'src/styles'),
+		},
+	},
 });
