898f2fce876fa64d149fc65065f319d5f149a889
Zahliada VM <waah777@gmail.com>
2025-08-30 20:27:10 +0300

    feat: добавить компонент Header с адаптивным дизайном и улучшенной семантикой

--- proj.portfolio/src/components/blocks/Header/Header.tsx
+++ proj.portfolio/src/components/blocks/Header/Header.tsx
@@ -1,10 +1,26 @@
+import './Header.scss';
+
 export function Header() {
 	console.log('## Header: render');
 	return (
-		<div className='header'>
-			<div className='container'>
-				<h1>Header Component</h1>
+		<section className='header'>
+			<div className='hamburger'>
+				<span></span>
+				<span></span>
+				<span></span>
 			</div>
-		</div>
+			<div className='container'>
+				<h3 className='title circle-big'>Меня зовут Александра Смит</h3>
+				<h1 className='title'>Я web-разработчик из города Москва</h1>
+				<div className='buttons-wrapper'>
+					<a className='button-primary' href='#portfolio'>
+						Портфолио
+					</a>
+					<a className='button' href='#about'>
+						Про меня
+					</a>
+				</div>
+			</div>
+		</section>
 	);
 }
