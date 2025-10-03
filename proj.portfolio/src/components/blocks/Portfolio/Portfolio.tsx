9025a3c09bc6cdb6a40feb92cfbc8bf685da63d7
Zahliada VM <waah777@gmail.com>
2025-10-03 14:54:01 +0300

    Добавить секцию Portfolio с карточками работ
    - Создать компонент `Portfolio` для отображения списка работ
    - Добавить стили `Portfolio.scss` для сетки и карточек
    - Реализовать адаптивную верстку для разных экранов
    - Добавить и обновить изображения для примеров работ

--- proj.portfolio/src/components/blocks/Portfolio/Portfolio.tsx
+++ proj.portfolio/src/components/blocks/Portfolio/Portfolio.tsx
@@ -1,9 +1,29 @@
+import { Title } from 'components/ui';
+import './Portfolio.scss';
+
 export function Portfolio() {
 	console.log('## Portfolio: render');
 	return (
 		<section className='portfolio' id='portfolio'>
 			<div className='container'>
-				<h1>Portfolio Component</h1>
+				<Title type={3} className='circle-big'>
+					Портфолио
+				</Title>
+				<Title type={2}>Мои работы</Title>
+				<div className='divider'></div>
+
+				<div className='portfolio__images'>
+					<img src='assets/img/works/uber.jpg' alt='uber' />
+					<img src='assets/img/works/pulse.jpg' alt='pulse' />
+					<img src='assets/img/works/bread.jpg' alt='bread' />
+					<img
+						src='assets/img/works/plans.jpg'
+						alt='plans'
+						className='grid-2w'
+					/>
+					<img src='assets/img/works/ipad.jpg' alt='ipad' />
+					<img src='assets/img/works/mac.jpg' alt='mac' className='grid-2h' />
+				</div>
 			</div>
 		</section>
 	);
